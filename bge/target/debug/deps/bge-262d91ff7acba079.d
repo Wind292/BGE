320f084fb4ef9c0c92e18b61895eee3fa060cad5
Wind292 <131552453+Wind292@users.noreply.github.com>
2024-04-05 13:43:06 -0400

    you suck lightning, better cargo init

--- bge/target/debug/deps/bge-262d91ff7acba079.d
+++
@@ -1,5 +0,0 @@
-C:\Users\benja\OneDrive\Documents\BGE\bge\target\debug\deps\libbge-262d91ff7acba079.rmeta: src\main.rs
-
-C:\Users\benja\OneDrive\Documents\BGE\bge\target\debug\deps\bge-262d91ff7acba079.d: src\main.rs
-
-src\main.rs:
--- bge/target/debug/deps/bge-6f35c9e940a5ad82.d
+++
@@ -1,5 +0,0 @@
-C:\Users\benja\OneDrive\Documents\BGE\bge\target\debug\deps\libbge-6f35c9e940a5ad82.rmeta: src\main.rs
-
-C:\Users\benja\OneDrive\Documents\BGE\bge\target\debug\deps\bge-6f35c9e940a5ad82.d: src\main.rs
-
-src\main.rs:
